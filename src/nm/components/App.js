e57391425f62d39d4eb46eadb17c635a023a55f3
Geoffrey0247 <18912227221@163.com>
2017-01-04 17:05:05 +0800

    Init.

---
+++ src/nm/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+    }
+
+    render() {
+        return(
+            <div className="nm-app">Netease Music</div>
+        );
+    }
+}
+
+export default App;
