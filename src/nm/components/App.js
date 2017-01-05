bc59b51f1c947f0275b09a7b58a60d47dde58810
Geoffrey0247 <18912227221@163.com>
2017-01-05 10:19:20 +0800

    build the init framework

--- src/nm/components/App.js
+++ src/nm/components/App.js
@@ -1,14 +1,40 @@
 import React, { Component } from 'react';
 
 class App extends Component {
-    constructor(props) {
+    constructor (props) {
         super(props);
+    }
+
+    static defaultProps = {
+
+    }
+
+    static propTypes = {
+
+    }
+
+    state = {
+
+    }
+
+    componentDidMount()
+    {
 
     }
 
     render() {
-        return(
-            <div className="nm-app">Netease Music</div>
+        return (
+            <div className="nm-app">
+                <header>
+                    <div className="logo"></div>
+                    <h1>网易云音乐</h1>
+                </header>
+                <main>
+                    <aside></aside>
+                    <section></section>
+                </main>
+                <footer></footer>
+            </div>
         );
     }
 }
